99b06e37d68eda9f921edb22fb3f3ed18eba68bb
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-18 16:30:15 +0530

    practice classes and static classes

--- classes/person.js
+++ classes/person.js
@@ -1,4 +1,28 @@
-// Your code here
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(
+      `Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`
+    );
+  }
+
+  static introducePeople(people) {
+    if (!Array.isArray(people)) {
+      throw new Error("introducePeople only takes an array as an argument.");
+    }
+
+    if (people.some((person) => !(person instanceof Person))) {
+      throw new Error("All items in array must be Person class instances.");
+    }
+
+    people.forEach((person) => person.introduce());
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
